1a2a4826c9dffe71546c5e9d2516e9f04e41e10b
aangairbender <gvoin16@gmail.com>
2026-03-28 11:57:35 +0900

    added copy to clipboard button for source code dialog

--- cg-arena-ui/src/components/ViewCodeDialog.tsx
+++ cg-arena-ui/src/components/ViewCodeDialog.tsx
@@ -1,6 +1,7 @@
 import { DialogProps } from "@hooks/useDialog";
 import { useTheme } from "@hooks/useTheme";
-import { Modal } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
+import { FaRegClipboard } from "react-icons/fa6";
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomOneLight, atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
@@ -13,6 +14,13 @@ const ViewCodeDialog = (dialog: DialogProps<ViewCodeDialogData>) => {
   const { theme } = useTheme();
   const style = theme === 'dark' ? atomOneDark : atomOneLight;
 
+  const copyToClipboard = async () => {
+    const text = dialog.data?.content;
+    if (text) {
+      await navigator.clipboard.writeText(text);
+    }
+  };
+
   const data = dialog.data;
   if (data === undefined) return null;
 
@@ -20,6 +28,9 @@ const ViewCodeDialog = (dialog: DialogProps<ViewCodeDialogData>) => {
     <Modal show={dialog.isOpen} onHide={dialog.hide} scrollable size="xl">
       <Modal.Header closeButton>
         <Modal.Title>{data.title}</Modal.Title>
+        <Button style={{marginLeft: 16}} variant="outline-info" onClick={copyToClipboard}>
+          <FaRegClipboard className="bi"/>
+        </Button>
       </Modal.Header>
       <Modal.Body className="p-0">
         <SyntaxHighlighter language="text" style={style} showLineNumbers>{data.content}</SyntaxHighlighter>
